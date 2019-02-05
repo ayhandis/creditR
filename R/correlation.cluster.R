d829b7daa0ffe22c1887c63f8d70b21cb0ba3ddb
ayhandis <34791463+ayhandis@users.noreply.github.com>
2019-02-05 22:05:57 +0300

    Add files via upload

--- R/correlation.cluster.R
+++ R/correlation.cluster.R
@@ -12,21 +12,21 @@
 #' @examples
 #' correlation.cluster(my_data,clustering_output, "variable_names","Groups")
 
-correlation.cluster <- function(data, clustering_data, variables, clusters){
+correlation_cluster <- function(data, clustering_data, variables, clusters){
   number_cluster <- which( colnames(clustering_data)==clusters)
   number_variables <- which( colnames(clustering_data)==variables)
   cluster_v <- matrix(data=NA,nrow=1,ncol=length(unique(clustering_data[,clusters])))
   cor_v <- matrix(data=NA,nrow=1,ncol=length(unique(clustering_data[,clusters])))
   for(i in 1:length(unique(clustering_data[,clusters]))){
-  cor_data <-   data[clustering_data[variable_clusters[,number_cluster] ==i,][,number_variables]]
-  korelasyon_degerleri <- cor(cor_data)
-  lower <- korelasyon_degerleri
-  lower[lower.tri(korelasyon_degerleri, diag = TRUE)] = 99
-  lower <- as.data.frame(lower)
-  lower1 <- as.list(lower)
-  lowerlist <- mean(lower[lower!=99])
-  cluster_v[,i] <- i
-  cor_v[,i] <- lowerlist
+    cor_data <-   data[clustering_data[clustering_data[,number_cluster] ==i,][,number_variables]]
+    korelasyon_degerleri <- cor(cor_data)
+    lower <- korelasyon_degerleri
+    lower[lower.tri(korelasyon_degerleri, diag = TRUE)] = 99
+    lower <- as.data.frame(lower)
+    lower1 <- as.list(lower)
+    lowerlist <- mean(lower[lower!=99])
+    cluster_v[,i] <- i
+    cor_v[,i] <- lowerlist
   }
   cor_summary<-as.data.frame(cbind(t(cluster_v), t(cor_v)))
   colnames(cor_summary) <- c("Clusters","Correlation")
